435a46c0278781755da38e954967b14ceb43eab3
Alexmwale <alexmwalec03@gmail.com>
2026-07-16 01:04:04 +0200

    Creating middleware/auth for authnticating

---
+++ src/middleware/auth.js
@@ -0,0 +1,10 @@
+export  const requiredata = (req, res, next) => {   
+ next();
+}
+
+export const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: err.message || "Internal Server Error",
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+   });
+}
